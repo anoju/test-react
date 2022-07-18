3eb3022914b0b0f20d20aa3b316e10518b9c5347
zun5761 <zun5761@gmail.com>
2022-07-18 16:02:28 +0900

    class 4

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,15 +9,70 @@ function App() {
   function getNum(){
     return 100;
   }
+  const posts = '강남고기맛집';
+  const [listItems, listItemsChage] = useState([
+    {title:'남자 코트 추천', like: 0, date: '5/12'},
+    {title:'강남 우동 맛집', like: 0, date: '4/02'},
+    {title:'리액트 스터디', like: 0, date: '3/22'}
+  ]);
+  // console.log(listItemsChage)
+  let [likeNum, likeNumChange] = useState(0);
+  function clickEvt(){
+    const num = likeNum + 1;
+    likeNumChange(num);
+  }
+  function clickEvt2(index){
+    const list = [...listItems];
+    const num = list[index].like + 1;
+    list[index].like = num;
+    listItemsChage(list);
+  }
+  function clickTxtChange(){
+    const list = [...listItems];
+    list[0].title  = '여자 코트 추천';
+    listItemsChage(list);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div className={ logoClass}>개발 blog</div>
       </div>
-      <h4 style={setStyle}> { helloTxt } </h4>
-      <h4> { getNum() } </h4>
+      <h2 style={setStyle}> { helloTxt } </h2>
+      <h2> { getNum() } </h2>
       <div style={{textAlign:'center'}}>
-        <img src={ logo } alt=""  style={{'max-width':'50%'}} />
+        <img src={ logo } alt=""  style={{maxWidth:'50%'}} />
+      </div>
+      <div className="list">
+        <div>
+          <h3>{posts}</h3>
+          <div><button onClick={clickEvt}>👍{likeNum}</button></div>
+        </div>
+        <p> 3/17 발행</p>
+      </div>
+      {/* <div className="list">
+        <h3>{listItems[0]} <span onClick={clickEvt}>👍</span> {likeNum}</h3>
+        <p> 3/17 발행</p>
+      </div>
+      <div className="list">
+        <h3>{listItems[1]}</h3>
+        <p> 3/17 발행</p>
+      </div>
+      <div className="list">
+        <h3>{listItems[2]}</h3>
+        <p> 3/17 발행</p>
+      </div> */}
+      {listItems.map((item, index) => (
+        <div className="list" key={index}>
+          <div>
+            <h3>{item.title}</h3>
+            <div><button onClick={() => clickEvt2(index)}>👍{item.like}</button></div>
+          </div>
+          <p>{item.date} 발행</p>
+        </div>
+      ))}
+      <div className="btn">
+        <button onClick={clickTxtChange}>텍스트 변경</button>
       </div>
     </div>
   );
