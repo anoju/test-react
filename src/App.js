283f3584a67008a91679e22816732c781f8d265c
zun5761 <zun5761@gmail.com>
2022-07-19 15:36:23 +0900

    생활코딩 1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,52 @@ import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <img src={logo} alt="" />
+        <h1>{this.props.title}</h1>
+        <sub>{this.props.sub}</sub>
+      </header>
+    );
+  }
+}
+
+class Navi extends Component {
+  render() {
+    return (
+      <nav className="nav">
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <sub>&copy;anoju</sub>
+      </footer>
+    );
+  }
+}
+
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <Header title="ANOJU!!" sub="리액트 연습중"></Header>
+        <Navi />
+        <main className="main">
+          Hello, react!!
+        </main>
+        <Footer />
       </div>
     );
   }
